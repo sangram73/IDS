fac5d7dd28fa6eb815facf6ae554d3fc99998934
sangram73 <pandasangram20@gmail.com>
2025-04-10 03:11:08 +0530

    creat alert sysyem

---
+++ HIds/alert_manager.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def show_alert(message):
+    alert_window = tk.Tk()
+    alert_window.withdraw()  # Hide the main window
+    messagebox.showinfo("Alert", message)  # Show the alert with the message
+    alert_window.destroy()  # Destroy the alert window after the message is shown
