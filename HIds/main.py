461ea53eb21ba4d79087041ff53a2f079fa95046
sangram73 <pandasangram20@gmail.com>
2025-04-10 03:10:38 +0530

    upadate main.py

---
+++ HIds/main.py
@@ -0,0 +1,8 @@
+from Core.file_monitor  import FileChangeLogger
+def main():
+    # Call the function to monitor processes
+    FileChangeLogger()
+
+print("test")
+if __name__ == "__main__":
+   main() 
